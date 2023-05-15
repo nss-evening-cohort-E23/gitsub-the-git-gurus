fa4859e362f73335b00422fca6846918ad48f76d
Daun Kim <yennakim11@gmail.com>
2023-05-14 19:51:37 -0500

    fixed card width

--- main.js
+++ main.js
@@ -199,7 +199,7 @@ const reposOnDom = (array) => {
   let pinnedRepoString = "";
   for (const item of array) {
     pinnedRepoString += `
-    <div class="card" style="width: 40rem;">
+    <div class="card" style="width: 40rem height: 10rem;">
       <div class="card-header">${item.repoName}</div>
       <div class="card-body">
         <div class="pinned-card-body">
@@ -216,7 +216,6 @@ const reposOnDom = (array) => {
 const repoListOnDom = (array) => {
   let domString = "";
   for (const repo of array) {
-
     domString += `<div class="card" id="repo-card" style="width: 40rem height: 10rem;">
     <div class="card-header">${repo.repoName}</div>
     <div class="card-body">
@@ -231,21 +230,20 @@ const repoListOnDom = (array) => {
   renderToDom("#repoList", domString);
 };
 
-
 //render package search bar to DOM
 const packageSearch = () => {
   domString = `
   <div class="input-group mb-3" id="packageSearch">
   <input type="text" class="form-control" id="searchInput" placeholder="Search Package Name...">
 </div>`;
-renderToDom("#packageSearch", domString);
+  renderToDom("#packageSearch", domString);
 };
 
 // render package cards to DOM
 const packageCards = (array) => {
   let domString = "";
   for (package of array) {
-    domString += `<div class="card" style="width: 40rem;">
+    domString += `<div class="card" style="width: 40rem height: 10rem;">
     <div class="card-header">${package.packageName}</div>
     <div class="card-body">
       <div class="package-card-body">
@@ -258,11 +256,10 @@ const packageCards = (array) => {
   renderToDom("#packagesContainer", domString);
 };
 
-
 const projectsList = (array) => {
   let domString = "";
-  for(const project of array) {
-    domString += `<div class="card" style="width: 40rem;">
+  for (const project of array) {
+    domString += `<div class="card" style="width: 40rem height: 10rem;">
     <div class="project-card-header"> ${project.projName} 
     <i class="bi bi-three-dots" id="dots"></i>
     </div>
@@ -274,8 +271,8 @@ const projectsList = (array) => {
         </div>
       </div>
     </div>
-  </div>`
-  };
+  </div>`;
+  }
   renderToDom("#projectsList", domString);
 };
 
@@ -332,8 +329,6 @@ const repoForm = () => {
   renderToDom("#repoForm", formString);
 };
 
-
-
 // pakage form render to dom
 const packageForm = () => {
   let formString = `<div class="card">
@@ -375,31 +370,29 @@ const newProject = () => {
   renderToDom("#addProject", formString);
 };
 
-
-
 const app = () => {
   document.addEventListener(
     "DOMContentLoaded",
     function () {
       profileCard();
-    newPinnedRepo();
-    reposOnDom(repo);
-    const pinnedRepoForm = document.getElementById("pinnedRepoForm");
-    const addPinnedRepo = (event) => {
-      event.preventDefault();
-      const newPinnedRepoObj = {
-        id: repo.length + 1,
-        repoName: document.getElementById("projectName").value,
-        repoDesc: document.getElementById("projectDesc").value,
-        repoStar: true,
+      newPinnedRepo();
+      reposOnDom(repo);
+      const pinnedRepoForm = document.getElementById("pinnedRepoForm");
+      const addPinnedRepo = (event) => {
+        event.preventDefault();
+        const newPinnedRepoObj = {
+          id: repo.length + 1,
+          repoName: document.getElementById("projectName").value,
+          repoDesc: document.getElementById("projectDesc").value,
+          repoStar: true,
+        };
+
+        repo.push(newPinnedRepoObj);
+        reposOnDom(repo);
+        pinnedRepoForm.reset();
       };
 
-      repo.push(newPinnedRepoObj);
-      reposOnDom(repo);
-      pinnedRepoForm.reset();
-    };
-
-    pinnedRepoForm.addEventListener("submit", addPinnedRepo);
+      pinnedRepoForm.addEventListener("submit", addPinnedRepo);
     },
     false
   );
@@ -423,44 +416,46 @@ const app = () => {
     };
 
     pinnedRepoForm.addEventListener("submit", addPinnedRepo);
- 
-  } if (document.URL.includes("repos.html")) {
+  }
+  if (document.URL.includes("repos.html")) {
     repoForm();
     profileCard();
     repoListOnDom(repo);
-    
+
     //create a new repository
     const repoFormId = document.getElementById("repoFormId");
 
     const createRepository = (e) => {
       e.preventDefault();
-      
+
       const newRepoObj = {
         id: repo.length + 1,
         repoName: document.getElementById("repoName").value,
         repoDesc: document.getElementById("repoDesc").value,
       };
-      
+
       repo.unshift(newRepoObj);
       repoListOnDom(repo);
       repoFormId.reset();
-      
     };
     //search for a repository (repos.html)
     const repoSearch = (event) => {
-    const eventLC = event.target.value.toLowerCase();
-    const searchResult = repo.filter(item =>
-    item.repoName.toLowerCase().includes(eventLC) ||
-    item.repoDesc.toLowerCase().includes(eventLC)
-  );
+      const eventLC = event.target.value.toLowerCase();
+      const searchResult = repo.filter(
+        (item) =>
+          item.repoName.toLowerCase().includes(eventLC) ||
+          item.repoDesc.toLowerCase().includes(eventLC)
+      );
 
       repoListOnDom(searchResult);
-    }
+    };
     //target search input and listen for keyup to search
-    document.querySelector('#searchInput').addEventListener('keyup', repoSearch);
-     
-//add an event listener to the form submit button and pass in callback function
-repoFormId.addEventListener('submit', createRepository);
+    document
+      .querySelector("#searchInput")
+      .addEventListener("keyup", repoSearch);
+
+    //add an event listener to the form submit button and pass in callback function
+    repoFormId.addEventListener("submit", createRepository);
   }
   if (document.URL.includes("packages.html")) {
     window.onload = () => {
@@ -468,73 +463,78 @@ repoFormId.addEventListener('submit', createRepository);
       packageSearch();
       packageForm();
       profileCard();
-      
-      const packForm = document.getElementById('packForm');
+
+      const packForm = document.getElementById("packForm");
       const newPackage = (e) => {
         e.preventDefault();
-          const newPackageObj = {
-            id: packageSet.length + 1,
-            packageName: document.getElementById('packageName').value,
-            packageDesc: document.getElementById('packageDesc').value,
-          }
-          packageSet.push(newPackageObj);
-          packageCards(packageSet);
-          packForm.reset();
+        const newPackageObj = {
+          id: packageSet.length + 1,
+          packageName: document.getElementById("packageName").value,
+          packageDesc: document.getElementById("packageDesc").value,
         };
-        packForm.addEventListener('submit', newPackage);
+        packageSet.push(newPackageObj);
+        packageCards(packageSet);
+        packForm.reset();
+      };
+      packForm.addEventListener("submit", newPackage);
 
-    // delete function
-    const deletePackage = document.getElementById('packagesContainer');
+      // delete function
+      const deletePackage = document.getElementById("packagesContainer");
 
-    deletePackage.addEventListener('click', (e) => {
-      const [, id] = e.target.id.split("--");
-      const packIndex = packageSet.findIndex(package => package.id === Number(id));
-      packageSet.splice(packIndex, 1);
-      packageCards(packageSet);
-    });
+      deletePackage.addEventListener("click", (e) => {
+        const [, id] = e.target.id.split("--");
+        const packIndex = packageSet.findIndex(
+          (package) => package.id === Number(id)
+        );
+        packageSet.splice(packIndex, 1);
+        packageCards(packageSet);
+      });
 
-    // package search function
-    const searchPackage = (e) => {
-      const eventLC = event.target.value.toLowerCase();
-      const packageSearchResult = packageSet.filter(item =>
-        item.packageName.toLowerCase().includes(eventLC) ||
-        item.packageDesc.toLowerCase().includes(eventLC)
+      // package search function
+      const searchPackage = (e) => {
+        const eventLC = event.target.value.toLowerCase();
+        const packageSearchResult = packageSet.filter(
+          (item) =>
+            item.packageName.toLowerCase().includes(eventLC) ||
+            item.packageDesc.toLowerCase().includes(eventLC)
         );
         packageCards(packageSearchResult);
+      };
+      document
+        .getElementById("packageSearch")
+        .addEventListener("keyup", searchPackage);
     };
-    document.getElementById('packageSearch').addEventListener('keyup', searchPackage);
   }
-  } if (document.URL.includes("projects.html")) {
+  if (document.URL.includes("projects.html")) {
     profileCard();
     newProject();
     projectsList(projects);
 
     const createProject = (e) => {
       e.preventDefault();
-    
+
       const newProjList = {
-        id:projects.length + 1,
-        projName: document.getElementById('projName').value,
-        projDesc: document.getElementById('projectDescription').value,
-      }
+        id: projects.length + 1,
+        projName: document.getElementById("projName").value,
+        projDesc: document.getElementById("projectDescription").value,
+      };
       projects.push(newProjList);
       projectsList(projects);
       createProject.reset();
     };
-    addProject.addEventListener('submit', createProject);
+    addProject.addEventListener("submit", createProject);
 
     const search = (event) => {
       const eventLC = event.target.value.toLowerCase();
-      const searchResult = projects.filter(project =>
-        project.projName.toLowerCase().includes(eventLC) ||
-        project.projDesc.toLowerCase().includes(eventLC)
-        );
-        projectsList(searchResult);
+      const searchResult = projects.filter(
+        (project) =>
+          project.projName.toLowerCase().includes(eventLC) ||
+          project.projDesc.toLowerCase().includes(eventLC)
+      );
+      projectsList(searchResult);
     };
-    document.querySelector('#searchInput').addEventListener('keyup', search);
-    
+    document.querySelector("#searchInput").addEventListener("keyup", search);
   }
 };
-
 
 app();
